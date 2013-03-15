0fa1264da21244ec5273cdc942ead55063dce486
Kenton Hamaluik <hamaluik@ualberta.ca>
2013-03-14 23:57:54 -0600

    Cleaning up [un]moved files

--- src/com/mcnsa/essentials/PermissionsManager.java
+++
@@ -1,8 +0,0 @@
-package com.mcnsa.essentials;
-
-public PermissionsManager {
-	public boolean playerHasPermission(EssentialsPlayer player, String permission) {
-		// TODO: implement PermissionsEx permissions
-		return false;
-	}
-};
--- src/com/mcnsa/essentials/managers/EventManager.java
+++
@@ -1,5 +0,0 @@
-package com.mcnsa.essentials.managers;
-
-public class EventManager {
-	
-};
