7f05830118a62771d3a9fa8df5f067bc3e77c432
Kenton Hamaluik <hamaluik@ualberta.ca>
2013-03-15 14:39:47 -0600

    Fixed instatiation exception bug on Debug component

--- src/com/mcnsa/essentials/components/Debug.java
+++ src/com/mcnsa/essentials/components/Debug.java
@@ -8,9 +8,11 @@ import org.bukkit.command.CommandSender;
 
 import com.mcnsa.essentials.MCNSAEssentials;
 import com.mcnsa.essentials.annotations.Command;
+import com.mcnsa.essentials.runnables.TPSTimerTask;
 import com.mcnsa.essentials.utilities.ColourHandler;
 
 public class Debug {
+	
 	@Command(command = "ping",
 			description = "pings the server to make sure it is still responding",
 			permissions = {"debug.ping"})
@@ -21,7 +23,7 @@ public class Debug {
 				"I heard " + sender.getName() + " likes boys!",
 				"I said a hip hop, the hippie, the hippie, to the hip hip hop, you don't stop!"
 		};
-		ColourHandler.sendMessage(sender, "&e" + (new Random()).nextInt(replies.length));
+		ColourHandler.sendMessage(sender, "&e" + replies[(new Random()).nextInt(replies.length)]);
 		return true;
 	}
 	
@@ -37,63 +39,20 @@ public class Debug {
 			description = "tells you the current ticks per second (should be 20.0)",
 			permissions = {"tps"})
 	// pretty much a copy of commandbook's TPS
-	public static boolean ticksPerSecond(final CommandSender sender, final float testTime) {
+	public static boolean ticksPerSecond(CommandSender sender, float testTime) {		
 		// alert them
 		ColourHandler.sendMessage(sender, "&9Timing clock test for " + testTime + " seconds...");
 		ColourHandler.sendMessage(sender, "&cDO NOT &9change the world time or do anything stressful during this test!");
 		
-		final World world = Bukkit.getServer().getWorlds().get(0);
-		final int expectedTicks = (int)(20.0f * testTime);
-		final long startTime = System.currentTimeMillis();
-		final long startTicks = world.getFullTime();
+		World world = Bukkit.getServer().getWorlds().get(0);
+		int expectedTicks = (int)(20.0f * testTime);
+		long startTime = System.currentTimeMillis();
+		long startTicks = world.getFullTime();
+		
+		TPSTimerTask task = new TPSTimerTask(sender, testTime, world, expectedTicks, startTime, startTicks);
 		
 		// now schedule the test
-		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MCNSAEssentials.getInstance(), new Runnable() {
-			@Override
-			public void run() {
-				long nowTime = System.currentTimeMillis();
-				long nowTicks = world.getFullTime();
-				
-				float elapsedSecs = (nowTime - startTime) / 1000.0f;
-				int elapsedTicks = (int)(nowTicks - startTicks);
-				
-				float error = ((int)(((testTime - elapsedSecs) / elapsedSecs * 100) * 10)) / 10.0f;
-				float clockRate = elapsedTicks / elapsedSecs;
-				
-				// make sure the bukkit scheduler is working properly
-				if(expectedTicks != elapsedTicks) {
-					ColourHandler.sendMessage(sender, "&eWARNING: Bukkit scheduler was innacurate; expected "
-							+ expectedTicks + ", got " + elapsedTicks + " ticks");
-				}
-				
-				// now see if we're accurate
-				ColourHandler.sendMessage(sender, "&9Current ticks per second: &f" + clockRate + " &9(" + error + "% error)");
-				if(Math.round(clockRate) == 20) {
-					ColourHandler.sendMessage(sender, "&9Rating: &aEXCELLENT");
-				}
-				else {
-					if(elapsedSecs > testTime) {
-						// clock is behind
-						if(clockRate < 5) {
-							ColourHandler.sendMessage(sender, "&9Rating: &4RIDICULOUSLY BAD");
-						}
-						else if(clockRate < 10) {
-							ColourHandler.sendMessage(sender, "&9Rating: &cVERY BAD");
-						}
-						else if(clockRate < 15) {
-							ColourHandler.sendMessage(sender, "&9Rating: &6POOR");
-						}
-						else {
-							ColourHandler.sendMessage(sender, "&9Rating: &eNOT BAD");
-						}
-					}
-					else {
-						// clock is ahead
-						ColourHandler.sendMessage(sender, "&9Rating: &dWEIRD (clock is AHEAD)");
-					}
-				}
-			}
-		}, expectedTicks);
+		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MCNSAEssentials.getInstance(), task, expectedTicks);
 		
 		return true;
 	}
@@ -119,9 +78,9 @@ public class Debug {
 				System.getProperty("java.vm.name"),
 				System.getProperty("java.vm.version")));
 		ColourHandler.sendMessage(sender, "&eAvailable processors: " + rt.availableProcessors());
-		ColourHandler.sendMessage(sender, "&eAvailable total memory: " + Math.floor(rt.maxMemory() / 1024.0 / 1024.0 / 1024.0) + " GB");
-		ColourHandler.sendMessage(sender, "&eJVM allocated memory: " + Math.floor(rt.totalMemory() / 1024.0 / 1024.0 / 1024.0) + " GB");
-		ColourHandler.sendMessage(sender, "&eFree allocated memory: " + Math.floor(rt.freeMemory() / 1024.0 / 1024.0 / 1024.0) + " GB");
+		ColourHandler.sendMessage(sender, "&eAvailable total memory: " + Math.floor(rt.maxMemory() / 1024.0 / 1024.0) + " MB");
+		ColourHandler.sendMessage(sender, "&eJVM allocated memory: " + Math.floor(rt.totalMemory() / 1024.0 / 1024.0) + " MB");
+		ColourHandler.sendMessage(sender, "&eFree allocated memory: " + Math.floor(rt.freeMemory() / 1024.0 / 1024.0) + " MB");
 		
 		return true;
 	}
---
+++ src/com/mcnsa/essentials/runnables/TPSTimerTask.java
@@ -0,0 +1,70 @@
+package com.mcnsa.essentials.runnables;
+
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+
+import com.mcnsa.essentials.utilities.ColourHandler;
+
+
+public class TPSTimerTask implements Runnable {
+	private CommandSender sender = null;
+	private float expectedTime = 0;
+	private World world = null;
+	private int expectedTicks = 0;
+	private long startTime = 0;
+	private long startTicks = 0;
+	
+	public TPSTimerTask(CommandSender sender, float expectedTime, World world, int expectedTicks, long startTime, long startTicks) {
+		this.sender = sender;
+		this.expectedTime = expectedTime;
+		this.world = world;
+		this.expectedTicks = expectedTicks;
+		this.startTime = startTime;
+		this.startTicks = startTicks;
+	}
+	
+	@Override
+	public void run() {
+		long nowTime = System.currentTimeMillis();
+		long nowTicks = world.getFullTime();
+		
+		float elapsedSecs = (nowTime - startTime) / 1000.0f;
+		int elapsedTicks = (int)(nowTicks - startTicks);
+		
+		float error = ((int)(((expectedTime - elapsedSecs) / elapsedSecs * 100) * 10)) / 10.0f;
+		float clockRate = elapsedTicks / elapsedSecs;
+		
+		// make sure the bukkit scheduler is working properly
+		if(expectedTicks != elapsedTicks) {
+			ColourHandler.sendMessage(sender, "&eWARNING: Bukkit scheduler was innacurate: expected "
+					+ expectedTicks + ", got " + elapsedTicks + " ticks");
+		}
+		
+		// now see if we're accurate
+		ColourHandler.sendMessage(sender, "&9Current ticks per second: &f" + clockRate + " &9(" + error + "% error)");
+		if(Math.round(clockRate) == 20) {
+			ColourHandler.sendMessage(sender, "&9Rating: &aEXCELLENT");
+		}
+		else {
+			if(elapsedSecs > expectedTime) {
+				// clock is behind
+				if(clockRate < 5) {
+					ColourHandler.sendMessage(sender, "&9Rating: &4RIDICULOUSLY BAD");
+				}
+				else if(clockRate < 10) {
+					ColourHandler.sendMessage(sender, "&9Rating: &cVERY BAD");
+				}
+				else if(clockRate < 15) {
+					ColourHandler.sendMessage(sender, "&9Rating: &6POOR");
+				}
+				else {
+					ColourHandler.sendMessage(sender, "&9Rating: &eNOT BAD");
+				}
+			}
+			else {
+				// clock is ahead
+				ColourHandler.sendMessage(sender, "&9Rating: &dWEIRD (clock is AHEAD)");
+			}
+		}
+	}
+}
