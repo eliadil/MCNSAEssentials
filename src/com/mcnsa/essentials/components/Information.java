020b94dc1c399123cf2bff7afbb1f0741ccb39b3
Kenton Hamaluik <hamaluik@ualberta.ca>
2013-03-15 13:38:24 -0600

    Added a basic /rules command to the information package.

--- src/com/mcnsa/essentials/components/Information.java
+++ src/com/mcnsa/essentials/components/Information.java
@@ -1,14 +1,24 @@
 package com.mcnsa.essentials.components;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
+import com.mcnsa.essentials.MCNSAEssentials;
 import com.mcnsa.essentials.annotations.Command;
 import com.mcnsa.essentials.utilities.ColourHandler;
 
-public class Information {
+public class Information implements Listener {
 	public static String motd = "Hello, %name%, welcome to MCNSA!\nThe time is now %time% and you're in world '%world%'";
 	public static String[] rules = {"Don't be a derp, you derp"};
+	
+	public Information() {
+		// register our events
+		Bukkit.getServer().getPluginManager().registerEvents(this, MCNSAEssentials.getInstance());
+	}
 	
 	private static String formatTime(long time) {
 		int hours = (int) Math.floor(time / 1000);
@@ -29,11 +39,30 @@ public class Information {
 		}
 	}
 	
+	private static void sendRules(CommandSender sender) {
+		for(int i = 0; i < rules.length; i++) {
+			ColourHandler.sendMessage(sender, (i+1) + ". " + rules[i]);
+		}
+	}
+	
+	// bukkit event handler on player join
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		sendMOTD(event.getPlayer());
+	}
+	
 	@Command(command = "motd",
 			description = "tells you the message of the day!",
 			playerOnly = true)
 	public static boolean motd(CommandSender sender) {
 		sendMOTD((Player)sender);
+		return true;
+	}
+	
+	@Command(command = "rules",
+			description = "lists the rules of the server")
+	public static boolean rules(CommandSender sender) {
+		sendRules(sender);
 		return true;
 	}
 }
