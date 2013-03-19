1c4e0a7e089fa12bbfeac56679666b96ff76b9a4
Kenton Hamaluik <hamaluik@ualberta.ca>
2013-03-18 20:06:15 -0600

    Added time setting command and player-in-world selection

--- src/com/mcnsa/essentials/components/Time.java
+++ src/com/mcnsa/essentials/components/Time.java
@@ -1,20 +1,29 @@
 package com.mcnsa.essentials.components;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.mcnsa.essentials.MCNSAEssentials;
 import com.mcnsa.essentials.annotations.Command;
 import com.mcnsa.essentials.annotations.ComponentInfo;
+import com.mcnsa.essentials.annotations.Setting;
 import com.mcnsa.essentials.exceptions.EssentialsCommandException;
 import com.mcnsa.essentials.utilities.ColourHandler;
+import com.mcnsa.essentials.utilities.PlayerSelector;
 import com.mcnsa.essentials.utilities.TimeFormat;
 
 @ComponentInfo(friendlyName = "Time",
 				description = "Allows you to query and set the world time",
 				permsSettingsPrefix = "time")
 public class Time {
+	@Setting(node = "dawn-offset-hours") public static long dawnOffsetHours = 8;
+	@Setting(node = "broadcast-time-changes") public static boolean broadcastTimeChanges = true;
+	@Setting(node = "broadcast-only-in-affected-world") public static boolean broadcastOnlyInAffectedWorld = true;
+	
 	@Command(command = "time",
 			aliases = {"thetime"},
 			description = "tells you the current time in your world",
@@ -37,6 +46,66 @@ public class Time {
 		
 		// tell them the time!
 		ColourHandler.sendMessage(sender, "&dThe current time in world '" + world.getName() + "' is: &f" + TimeFormat.formatMinecraftTime(world.getTime()));
+		
+		return true;
+	}
+
+	@Command(command = "settime",
+			description = "sets the current time in your current world",
+			permissions = {"set"},
+			playerOnly = true)
+	public static boolean setTime(CommandSender sender, String newTime) throws EssentialsCommandException {
+		return setTime(sender, ((Player)sender).getWorld().getName(), newTime);
+	}
+
+	@Command(command = "settime",
+			arguments = {"world"},
+			description = "sets the current time in the given world",
+			permissions = {"set"})
+	public static boolean setTime(CommandSender sender, String worldName, String newTime) throws EssentialsCommandException {
+		// get the world
+		World targetWorld = Bukkit.getServer().getWorld(worldName);
+		if(targetWorld == null) {
+			throw new EssentialsCommandException("I don't know what universe you're living in, but this one doesn't have the world '%s' in it!", worldName);
+		}
+		
+		// ok, parse the time string
+		long time = TimeFormat.parseTime(newTime);
+		
+		// and set it!
+		targetWorld.setTime(time);
+		
+		// get a formatted new time
+		newTime = TimeFormat.formatMinecraftTime(time);
+		
+		// alert!
+		if(broadcastTimeChanges) {
+			// get our affected players
+			ArrayList<Player> targetPlayers = null;
+			if(broadcastOnlyInAffectedWorld) {
+				targetPlayers = PlayerSelector.selectPlayersExact(String.format("world:%s;*", targetWorld.getName()));
+			}
+			else {
+				targetPlayers = PlayerSelector.selectPlayersExact("*");
+			}
+			
+			// broadcast it!
+			for(Player targetPlayer: targetPlayers) {
+				ColourHandler.sendMessage(targetPlayer, "&6The time in world '%s' has been changed to: %s by %s",
+						targetWorld.getName(),
+						newTime,
+						sender.getName());
+			}
+		}
+		
+		// and log it
+		MCNSAEssentials.log("&6The time in world '%s' has been changed to: %s by %s",
+				targetWorld.getName(),
+				newTime,
+				sender.getName());
+		ColourHandler.sendMessage(sender, "&6You have changed the time in world '%s' to %s!",
+				targetWorld.getName(),
+				newTime);
 		
 		return true;
 	}
--- src/com/mcnsa/essentials/utilities/PlayerSelector.java
+++ src/com/mcnsa/essentials/utilities/PlayerSelector.java
@@ -5,17 +5,49 @@ import java.util.Arrays;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+
+import com.mcnsa.essentials.exceptions.EssentialsCommandException;
 
 public class PlayerSelector {
 	public static OfflinePlayer selectSinglePlayer(String target) {
 		return Bukkit.getServer().getOfflinePlayer(target);
 	}
 	
-	public static ArrayList<Player> selectPlayersExact(String target) {
+	public static ArrayList<Player> selectPlayersExact(String target) throws EssentialsCommandException {
 		ArrayList<Player> matchedPlayers = new ArrayList<Player>();
 		if(target.equals("*")) {
 			matchedPlayers = new ArrayList<Player>(Arrays.asList(Bukkit.getServer().getOnlinePlayers()));
+		}
+		else if(target.startsWith("world:")) {
+			// split it into half
+			String[] parts = target.split(";", 2);
+			if(parts.length != 2) {
+				throw new EssentialsCommandException("Player-in-world selection format: world:<worldname>;<player[s]>");
+			}
+			
+			// ok, get the world name
+			String[] worldParts = parts[0].split(":", 2);
+			if(worldParts.length != 2) {
+				throw new EssentialsCommandException("Player-in-world selection format: world:<worldname>;<player[s]>");
+			}
+			// try to get the world
+			World targetWorld = Bukkit.getServer().getWorld(worldParts[1]);
+			if(targetWorld == null) {
+				throw new EssentialsCommandException("I couldn't find the world '%s' to select from!", worldParts[1]);
+			}
+			
+			// ok, world exists
+			// get all players that we're targeting
+			ArrayList<Player> players = selectPlayersExact(parts[1]);
+			
+			// go through and make sure that the players are in the correct world
+			for(Player player: players) {
+				if(player.getWorld().equals(targetWorld)) {
+					matchedPlayers.add(player);
+				}
+			}
 		}
 		else {
 			String[] playerTargets = target.split(",");
@@ -27,6 +59,10 @@ public class PlayerSelector {
 				}
 			}
 		}
+		
+		/*if(matchedPlayers.isEmpty()) {
+			throw new EssentialsCommandException("I couldn't find the player[s] '%s'! For information on player selecting, type /helpess playerselecting", target);
+		}*/
 		
 		return matchedPlayers;
 	}
