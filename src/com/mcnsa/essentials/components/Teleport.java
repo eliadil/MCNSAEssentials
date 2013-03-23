68049333a1212797aa13a644a89dfcb16ea382d5
Kenton Hamaluik <hamaluik@ualberta.ca>
2013-03-23 11:34:02 -0600

    Folding TeleportHistory into teleport, adding multiple history sites

--- src/com/mcnsa/essentials/components/Teleport.java
+++ src/com/mcnsa/essentials/components/Teleport.java
@@ -2,15 +2,24 @@ package com.mcnsa.essentials.components;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 
+import com.mcnsa.essentials.MCNSAEssentials;
 import com.mcnsa.essentials.annotations.Command;
 import com.mcnsa.essentials.annotations.ComponentInfo;
+import com.mcnsa.essentials.annotations.Setting;
 import com.mcnsa.essentials.exceptions.EssentialsCommandException;
 import com.mcnsa.essentials.utilities.ColourHandler;
 import com.mcnsa.essentials.utilities.PlayerSelector;
@@ -18,7 +27,155 @@ import com.mcnsa.essentials.utilities.PlayerSelector;
 @ComponentInfo(friendlyName = "Teleport",
 				description = "Various teleportation commands",
 				permsSettingsPrefix = "teleport")
-public class Teleport {
+public class Teleport implements Listener {
+	@Setting(node = "max-teleport-history") public static int MAX_TELEPORT_HISTORY = 5;
+	
+	public Teleport() {
+		// register our events
+		Bukkit.getServer().getPluginManager().registerEvents(this, MCNSAEssentials.getInstance());
+	}
+	
+	// metadata utility functions
+	private static boolean ignoringTeleport(Player player) {
+		if(!player.hasMetadata("ignoreTP") || player.getMetadata("ignoreTP").size() != 1) {
+			return false;
+		}
+		return player.getMetadata("ignoreTP").get(0).asBoolean();
+	}
+	
+	private static void ignoreTeleport(Player player, boolean ignore) {
+		if(ignore) {
+			player.setMetadata("ignoreTP", new FixedMetadataValue(MCNSAEssentials.getInstance(), true));
+		}
+		else {
+			player.removeMetadata("ignoreTP", MCNSAEssentials.getInstance());
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void addTeleportHistory(Player player, Location location) throws EssentialsCommandException {
+		LinkedList<Location> history = null;
+		if(player.hasMetadata("tpHistory")) {
+			if(player.getMetadata("tpHistory").get(0).value() instanceof LinkedList<?>) {
+				history = (LinkedList<Location>)player.getMetadata("tpHistory").get(0).value();
+			}
+			else {
+				throw new EssentialsCommandException("Error: something went horribly wrong!");
+			}
+		}
+		else {
+			history = new LinkedList<Location>();
+		}
+		
+		// update their history
+		history.add(location);
+		
+		// make sure it doesn't get too big
+		if(history.size() > MAX_TELEPORT_HISTORY) {
+			history.remove();
+		}
+		
+		// update their metadata
+		player.setMetadata("tpHistory", new FixedMetadataValue(MCNSAEssentials.getInstance(), history));
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static Location getLastLocation(Player player) throws EssentialsCommandException {
+		LinkedList<Location> history = null;
+		if(player.hasMetadata("tpHistory")) {
+			if(player.getMetadata("tpHistory").get(0).value() instanceof LinkedList<?>) {
+				history = (LinkedList<Location>)player.getMetadata("tpHistory").get(0).value();
+			}
+			else {
+				throw new EssentialsCommandException("Error: something went horribly wrong!");
+			}
+		}
+		else {
+			throw new EssentialsCommandException("You don't have anywhere to go!");
+		}
+		
+		// make sure we have somewhere to go
+		if(history.size() == 0) {
+			player.removeMetadata("tpHistory", MCNSAEssentials.getInstance());
+			throw new EssentialsCommandException("You don't have anywhere to go!");
+		}
+		
+		// get our last TP location, popping it off
+		Location lastLocation = history.pop();
+		
+		// update their metadata
+		player.setMetadata("tpHistory", new FixedMetadataValue(MCNSAEssentials.getInstance(), history));
+		
+		// and return
+		return lastLocation;
+	}
+	
+	/*private static void setLastLocation(Player player, Location location) {
+		player.setMetadata("tpHistory", new FixedMetadataValue(MCNSAEssentials.getInstance(), location));
+	}
+	
+	private static void removeLastLocation(Player player) {
+		player.removeMetadata("tpHistory", MCNSAEssentials.getInstance());
+	}
+	
+	private static Location getLastLocation(Player player) throws EssentialsCommandException {
+		if(player.getMetadata("tpHistory").size() != 1) {
+			throw new EssentialsCommandException("tpHistory != 1");
+		}
+		MetadataValue mv = player.getMetadata("tpHistory").get(0);
+		return (Location)mv.value();
+	}*/
+	
+	// our bukkit event handlers
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onPlayerTeleport(PlayerTeleportEvent event) throws EssentialsCommandException {
+		if(!ignoringTeleport(event.getPlayer())) {
+			// set their last location
+			addTeleportHistory(event.getPlayer(), event.getPlayer().getLocation());
+		}
+		else {
+			// and re-enable teleport logging
+			ignoreTeleport(event.getPlayer(), false);
+		}
+		
+		// disable fall distance
+		event.getPlayer().setFallDistance(0);
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerRespawn(PlayerRespawnEvent event) throws EssentialsCommandException {
+		// set their last location
+		addTeleportHistory(event.getPlayer(), event.getPlayer().getLocation());
+	}
+	
+	// our commands
+	@Command(command = "back",
+			aliases = {"return"},
+			description = "sends you back to where you were before you last teleported",
+			permissions = {"back"},
+			playerOnly = true)
+	public static boolean back(CommandSender sender) throws EssentialsCommandException {
+		// get our player
+		Player player = (Player)sender;
+		
+		// make sure we have somewhere else to go
+		if(!player.hasMetadata("tpHistory")) {
+			throw new EssentialsCommandException("You don't have anywhere to go!");
+		}
+		
+		// get their last location
+		Location lastLocation = getLastLocation(player);
+		
+		// disable teleport logging
+		ignoreTeleport(player, true);
+		
+		// and send them there
+		player.teleport(lastLocation);
+		ColourHandler.sendMessage(player, "&6!hsooW");
+		
+		return true;
+	}
+	
 	@Command(command = "tp",
 			aliases = {"teleport"},
 			arguments = {"player"},
@@ -26,7 +183,7 @@ public class Teleport {
 			permissions = { "self" },
 			playerOnly = true)
 	public static boolean teleport(CommandSender sender, String targetPlayer) throws EssentialsCommandException {
-		return teleport(sender, sender.getName(), targetPlayer);
+		return teleport(sender, sender.getName(), /*"to", */targetPlayer);
 	}
 	
 	@Command(command = "bring",
@@ -35,7 +192,7 @@ public class Teleport {
 			permissions = { "teleport.bring" },
 			playerOnly = true)
 	public static boolean bring(CommandSender sender, String targetPlayer) throws EssentialsCommandException {
-		teleport(sender, targetPlayer, sender.getName());
+		teleport(sender, targetPlayer, /*"to", */sender.getName());
 		return true;
 	}
 	
@@ -44,7 +201,13 @@ public class Teleport {
 			arguments = {"target player[s]", "destination player"},
 			description = "teleports <target player[s]> to <destination player>",
 			permissions = { "other" })
-	public static boolean teleport(CommandSender sender, String targetPlayer, String destinationPlayer) throws EssentialsCommandException {
+	public static boolean teleport(CommandSender sender, String targetPlayer, /*String to, */String destinationPlayer) throws EssentialsCommandException {
+		// ridddles request
+		// match our 'to'
+		/*if(!to.equalsIgnoreCase("to")) {
+			throw new EssentialsCommandException("%s should be 'to'!", to);
+		}*/
+		
 		// try to find the destination player
 		Player destination = Bukkit.getServer().getPlayer(destinationPlayer);
 		if(destination == null) {
