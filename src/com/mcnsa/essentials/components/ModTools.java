220495dedf1e5e9f934508917e24b06708b86ac9
Kenton Hamaluik <hamaluik@ualberta.ca>
2013-03-24 10:39:37 -0600

    Added very basic player inventory opening and translations

--- src/com/mcnsa/essentials/components/ModTools.java
+++ src/com/mcnsa/essentials/components/ModTools.java
@@ -10,6 +10,7 @@ import org.bukkit.metadata.FixedMetadataValue;
 import com.mcnsa.essentials.MCNSAEssentials;
 import com.mcnsa.essentials.annotations.Command;
 import com.mcnsa.essentials.annotations.ComponentInfo;
+import com.mcnsa.essentials.annotations.Translation;
 import com.mcnsa.essentials.exceptions.EssentialsCommandException;
 import com.mcnsa.essentials.managers.PermissionsManager;
 import com.mcnsa.essentials.utilities.ColourHandler;
@@ -19,18 +20,39 @@ import com.mcnsa.essentials.utilities.StringUtils;
 @ComponentInfo(friendlyName = "ModTools",
 				description = "Various tools for moderating not included elsewhere",
 				permsSettingsPrefix = "modtools")
-public class ModTools {	
+public class ModTools {
 	// our commands
-	/*@Command(command = "openinv",
+	@Translation(node = "openinv.only-online") public static String onlyOnline = "Sorry, you can only open inventories of online players";
+	@Translation(node = "openinv.opened-inventory") public static String openedInventory = "&9You have opened %player%'s inventory!";
+	@Command(command = "openinv",
 			aliases = {"openinventory", "inventory"},
 			arguments = {"target player"},
 			description = "opens that target player's inventory",
 			permissions = {"openinv"},
 			playerOnly = true)
-	public static boolean openInventory(CommandSender sender, String targetPlayer) {
+	public static boolean openInventory(CommandSender sender, String targetPlayer) throws EssentialsCommandException {
+		// get our player
+		Player player = (Player)sender;
+		
+		// get our target player
+		Player target = Bukkit.getServer().getPlayer(targetPlayer);
+		if(target == null) {
+			throw new EssentialsCommandException(onlyOnline);
+		}
+		
+		// now open the player's inventory
+		player.openInventory(target.getInventory());
+		
+		ColourHandler.sendMessage(sender, openedInventory.replaceAll("%player%", target.getName()));
+		
 		return true;
-	}*/
+	}
 	
+	@Translation(node = "sudo.invalid-targets") public static String invalidTargets = "I couldn't find target player[s] '%targetPlayers%' to sudo!";
+	@Translation(node = "sudo.exempt") public static String exempt = "&c%player% is exempt from sudo!";
+	@Translation(node = "sudo.made-to-run-command") public static String madeToRunCommand = "&3%player% made you run the command: %command%";
+	@Translation(node = "sudo.not-valid-command") public static String notValidCommand = "Not a valid command: %command%";
+	@Translation(node = "sudo.has-run-command") public static String hasRunCommand = "&3%player% has run your command!";
 	@Command(command = "sudo",
 			arguments = {"target player[s]", "command"},
 			description = "causes another player to execute a command",
@@ -41,7 +63,7 @@ public class ModTools {
 		
 		// make sure we have at least one target player
 		if(targetPlayers.size() == 0) {
-			throw new EssentialsCommandException("I couldn't find target player[s] '%s' to sudo!", targetPlayer);
+			throw new EssentialsCommandException(invalidTargets.replaceAll("%targetPlayers%", targetPlayer));
 		}
 		
 		// build our command
@@ -54,12 +76,12 @@ public class ModTools {
 		for(Player player: targetPlayers) {
 			// see if they're exempt from sudo
 			if(PermissionsManager.playerHasPermission(player, "modtools.sudo.exempt")) {
-				ColourHandler.sendMessage(sender, "&c%s is exempt from sudo!", player.getName());
+				ColourHandler.sendMessage(sender, exempt.replaceAll("%player%", player.getName()));
 				continue;
 			}
 			
 			// warn them
-			ColourHandler.sendMessage(player, "&3%s made you run the command: %s", sender.getName(), command);
+			ColourHandler.sendMessage(player, madeToRunCommand.replaceAll("%player%", sender.getName()).replaceAll("%command%", command));
 			
 			// ok, execute the command
 			// but disable permissions checking first
@@ -68,10 +90,10 @@ public class ModTools {
 			player.removeMetadata("ignorePermissions", MCNSAEssentials.getInstance());
 			
 			if(!result) {
-				throw new EssentialsCommandException("Not a valid command: %s", command);
+				throw new EssentialsCommandException(notValidCommand.replaceAll("%command%", command));
 			}
 			else {
-				ColourHandler.sendMessage(sender, "&3%s has run your command!", player.getName());
+				ColourHandler.sendMessage(sender, hasRunCommand.replaceAll("%player%", player.getName()));
 			}
 		}
 		
